16b963581eacd6a7dbd8734671e8d214de2d5087
LennynT219 <lennynvega35@gmail.com>
2026-03-29 15:57:40 -0500

    add: implementacion de envio de correo

--- pagolink/settings.py
+++ pagolink/settings.py
@@ -115,5 +115,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Configuración email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Configuración email (Gmail SMTP)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
